4b6877be176298bfdb77f5317568d42b2c8f9274
Viktor Martynov <2013alfabravo@gmail.com>
2022-10-09 19:24:13 +0400

    annotation config example

--- src/main/java/com/github/alfabravo2013/AppConfig.java
+++ src/main/java/com/github/alfabravo2013/AppConfig.java
@@ -1,33 +1,35 @@
 package com.github.alfabravo2013;
 
 import com.github.alfabravo2013.entities.BaseballGame;
-import com.github.alfabravo2013.entities.Cubs;
 import com.github.alfabravo2013.entities.Game;
-import com.github.alfabravo2013.entities.RedSox;
 import com.github.alfabravo2013.entities.Team;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
-@Import(InfrastructureConfig.class)
+@ComponentScan(basePackages = "com.github.alfabravo2013")
 public class AppConfig {
 
-    @Bean
-    public Team redSox() {
-        return new RedSox();
-    }
+    @Resource
+    private DataSource dataSource;
+
+    @Autowired
+    @Qualifier("redSox")
+    private Team home;
+
+    @Autowired
+    @Qualifier("cubs")
+    private Team away;
 
     @Bean
-    public Team cubs() {
-        return new Cubs();
-    }
-
-    @Bean
-    public Game game(DataSource dataSource) {
-        var game = new BaseballGame(redSox(), cubs());
+    public Game game() {
+        var game = new BaseballGame(home, away);
         game.setDataSource(dataSource);
         return game;
     }
--- src/main/java/com/github/alfabravo2013/entities/Cubs.java
+++ src/main/java/com/github/alfabravo2013/entities/Cubs.java
@@ -1,5 +1,8 @@
 package com.github.alfabravo2013.entities;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Cubs implements Team {
 
     @Override
