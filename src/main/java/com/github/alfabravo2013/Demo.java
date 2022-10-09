52d023e345eb29c7fb9c2c8e887c5d5621cac07e
Viktor Martynov <2013alfabravo@gmail.com>
2022-10-09 20:03:35 +0400

    XML config example

--- src/main/java/com/github/alfabravo2013/Demo.java
+++ src/main/java/com/github/alfabravo2013/Demo.java
@@ -3,11 +3,12 @@ package com.github.alfabravo2013;
 import com.github.alfabravo2013.entities.BaseballGame;
 import com.github.alfabravo2013.entities.Game;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Demo {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        // ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         var game = ctx.getBean("game", Game.class);
         System.out.println(game.playGame());
